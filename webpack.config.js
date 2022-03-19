2b2f2be4a8d1ea439f312760275d974cdf6cd305
VinogradovaD <darina_1999@inbox.ru>
2022-03-19 18:37:20 +0300

    webpack fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+//const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry:'./js/index.js',
   output: {
     filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './html/index.html'
+      template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CopyPlugin({
+      patterns: [
+          {
+            from: path.resolve(__dirname, 'src/img'),
+            to:   path.resolve(__dirname, 'dist/img')
+          }
+        ]
+      })
+   // new CleanWebpackPlugin()
   ],
+  module: {
+    rules: [
+      // { 
+      //   test: /\.css$/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader']
+      // },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
   devServer: {
     port: 4200
   }
