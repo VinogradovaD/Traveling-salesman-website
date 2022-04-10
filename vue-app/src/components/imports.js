f4eb593773e6bc268d1b23165f51ce5e78eb36c3
VinogradovaD <darina_1999@inbox.ru>
2022-04-10 10:49:17 +0300

    add vue

--- vue-app/src/components/imports.js
+++
@@ -1,9 +0,0 @@
-import MethodCard from '@/components/MethodCard.vue'
-import VFooter from '@/components/VFooter.vue'
-import VButton from '@/components/VButton.vue'
-
-export default [
-  MethodCard,
-  VFooter,
-  VButton
-];
