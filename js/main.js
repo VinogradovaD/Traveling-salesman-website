00d253051d72830766be3da9f6316a9e529e485b
Darina Vinogradova <darina.vinogradova@maximumtest.ru>
2024-06-14 17:26:06 +0200

    New version on english

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const aboutSection = document.getElementById('about');
-const methodsSection = document.getElementById('methods');
-const taskSection = document.getElementById('task');
-const solvePageBtn = document.getElementById('solve-page-btn');
-const mainPageMethodsSection = document.getElementById('main-page-methods');
-const mainPageBtn = document.getElementById('main-page-btn');
-const methodsPageBtn = document.getElementById('methods-page-btn');
-const cityCountInput = document.getElementById('city-count');
-const errorMessage = document.getElementById('error-message');
-const matrixInputs = document.getElementById('matrix-inputs');
-const btnAnswer = document.getElementById('btn-answer');
-const btnClean = document.getElementById('btn-clean');
-const selectMethod = document.getElementById('select-method');
-const resultField = document.getElementById('result-field');
-const useMethodBtns = document.querySelectorAll('.use-method-btn');
+const aboutSection = document.getElementById("about");
+const methodsSection = document.getElementById("methods");
+const taskSection = document.getElementById("task");
+const solvePageBtn = document.getElementById("solve-page-btn");
+const mainPageMethodsSection = document.getElementById("main-page-methods");
+const mainPageBtn = document.getElementById("main-page-btn");
+const methodsPageBtn = document.getElementById("methods-page-btn");
+const cityCountInput = document.getElementById("city-count");
+const errorMessage = document.getElementById("error-message");
+const matrixInputs = document.getElementById("matrix-inputs");
+const btnAnswer = document.getElementById("btn-answer");
+const btnClean = document.getElementById("btn-clean");
+const selectMethod = document.getElementById("select-method");
+const resultField = document.getElementById("result-field");
+const useMethodBtns = document.querySelectorAll(".use-method-btn");
 
 let cityCount = 0;
 let matrixCountElements = 0;
@@ -30,7 +30,7 @@ function pathLength(path, distanceMatrix) {
   let length = 0;
 
   for (let i = 0; i < path.length - 1; i++) {
-    length += (Number)(distanceMatrix[path[i + 1] - 1][path[i] - 1]);
+    length += Number(distanceMatrix[path[i + 1] - 1][path[i] - 1]);
   }
 
   return length;
@@ -50,7 +50,7 @@ function bruteForce(distanceMatrix) {
   let minDistance = Number.MAX_VALUE;
   let bestPath = [];
 
-  allPaths.forEach(el => {
+  allPaths.forEach((el) => {
     if (pathLength(el, distanceMatrix) < minDistance) {
       minDistance = pathLength(el, distanceMatrix);
       bestPath = el;
@@ -69,7 +69,6 @@ function allCombinations(n) {
 
   function run(level) {
     for (let i = 0; i < n; i++) {
-
       if (!indices[i]) {
         indices[i] = true;
         combination[level] = i + 1;
@@ -77,7 +76,7 @@ function allCombinations(n) {
         if (level < n - 1) {
           run(level + 1, i + 1);
         } else {
-          result.push(([]).concat(combination).concat(combination[0]));
+          result.push([].concat(combination).concat(combination[0]));
         }
         indices[i] = false;
       }
@@ -114,7 +113,11 @@ function nearestCity(currentCity, currentPath, distanceMatrix) {
   let nextCity;
 
   for (let i = 0; i < distanceMatrix.length; i++) {
-    if (distanceMatrix[i][currentCity - 1] < min && !!distanceMatrix[i][currentCity - 1] && existInArray((i + 1), currentPath) !== -1) {
+    if (
+      distanceMatrix[i][currentCity - 1] < min &&
+      !!distanceMatrix[i][currentCity - 1] &&
+      existInArray(i + 1, currentPath) !== -1
+    ) {
       min = distanceMatrix[i][currentCity - 1];
       nextCity = i + 1;
     }
@@ -132,7 +135,6 @@ function existInArray(element, array) {
   return 1;
 }
 
-
 function nearestNeighbor2(distanceMatrix) {
   const start = performance.now();
   let minDistance = Number.MAX_VALUE;
@@ -145,7 +147,7 @@ function nearestNeighbor2(distanceMatrix) {
       bestPath = currentPath.path;
       minDistance = currentPath.distance;
     }
-  };
+  }
 
   const end = performance.now();
 
@@ -153,7 +155,9 @@ function nearestNeighbor2(distanceMatrix) {
 }
 
 function generateAnswer(result) {
-  return `Маршрут: ${result.path.join('->')}<br>Длина маршрута: ${result.distance}<br>Время работы алгоритма: ${result.time}мс`;
+  return `Маршрут: ${result.path.join("->")}<br>Длина маршрута: ${
+    result.distance
+  }<br>Время работы алгоритма: ${result.time}мс`;
 }
 
 function drawGraph() {
@@ -165,7 +169,6 @@ function drawGraph() {
 
       let that = {
         init: function (system) {
-
           particleSystem = system;
           particleSystem.screenSize(canvas.width, canvas.height);
           particleSystem.screenPadding(20);
@@ -173,33 +176,34 @@ function drawGraph() {
         },
 
         redraw: function () {
-
           ctx.fillStyle = "white";
           ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-          particleSystem.eachEdge(
-            function (edge, pt1, pt2) {
-              ctx.strokeStyle = "rgba(0,0,0, .333)";
-              ctx.lineWidth = 1;
-              ctx.beginPath();
-              ctx.moveTo(pt1.x, pt1.y);
-              ctx.lineTo(pt2.x, pt2.y);
-              ctx.stroke();
+          particleSystem.eachEdge(function (edge, pt1, pt2) {
+            ctx.strokeStyle = "rgba(0,0,0, .333)";
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(pt1.x, pt1.y);
+            ctx.lineTo(pt2.x, pt2.y);
+            ctx.stroke();
 
-              ctx.fillStyle = "black";
-              ctx.font = 'italic 13px sans-serif';
-              ctx.fillText(edge.data.name, (pt1.x + pt2.x) / 2, (pt1.y + pt2.y) / 2);
-            });
+            ctx.fillStyle = "black";
+            ctx.font = "italic 13px sans-serif";
+            ctx.fillText(
+              edge.data.name,
+              (pt1.x + pt2.x) / 2,
+              (pt1.y + pt2.y) / 2
+            );
+          });
 
-          particleSystem.eachNode(
-            function (node, pt) {
-              let w = 10;
-              ctx.fillStyle = "red";
-              ctx.fillRect(pt.x - w / 2, pt.y - w / 2, w, w);
-              ctx.fillStyle = "black";
-              ctx.font = 'italic 13px sans-serif';
-              ctx.fillText(node.name, pt.x + 8, pt.y + 8);
-            });
+          particleSystem.eachNode(function (node, pt) {
+            let w = 10;
+            ctx.fillStyle = "red";
+            ctx.fillRect(pt.x - w / 2, pt.y - w / 2, w, w);
+            ctx.fillStyle = "black";
+            ctx.font = "italic 13px sans-serif";
+            ctx.fillText(node.name, pt.x + 8, pt.y + 8);
+          });
         },
 
         initMouseHandling: function () {
@@ -212,8 +216,8 @@ function drawGraph() {
               if (dragged && dragged.node !== null) {
                 dragged.node.fixed = true;
               }
-              $(canvas).bind('mousemove', handler.dragged);
-              $(window).bind('mouseup', handler.dropped);
+              $(canvas).bind("mousemove", handler.dragged);
+              $(window).bind("mouseup", handler.dropped);
               return false;
             },
             dragged: function (e) {
@@ -231,19 +235,18 @@ function drawGraph() {
               if (dragged === null || dragged.node === undefined) return;
               if (dragged.node !== null) dragged.node.fixed = false;
               dragged = null;
-              $(canvas).unbind('mousemove', handler.dragged);
-              $(window).unbind('mouseup', handler.dropped);
+              $(canvas).unbind("mousemove", handler.dragged);
+              $(window).unbind("mouseup", handler.dropped);
               _mouseP = null;
               return false;
-            }
-          }
+            },
+          };
 
           $(canvas).mousedown(handler.clicked);
         },
-
-      }
+      };
       return that;
-    }
+    };
 
     $(document).ready(function () {
       sys = arbor.ParticleSystem(1000);
@@ -256,17 +259,18 @@ function drawGraph() {
         sys.addNode(result.path[i]);
       }
       for (let i = 0; i < cityCount; i++) {
-        sys.addEdge(sys.getNode(result.path[i]), sys.getNode(result.path[i + 1]), { name: matrix[result.path[i + 1] - 1][result.path[i] - 1] });
+        sys.addEdge(
+          sys.getNode(result.path[i]),
+          sys.getNode(result.path[i + 1]),
+          { name: matrix[result.path[i + 1] - 1][result.path[i] - 1] }
+        );
       }
-    })
-
-  })(this.jQuery)
+    });
+  })(this.jQuery);
 }
-
 
 //Считаем ответ
 function getAnswer() {
-
   if (!cityCount) {
     errorMessage.innerHTML = "Введите количество городов";
   } else if (matrixCountElements < cityCount * cityCount) {
@@ -275,25 +279,25 @@ function getAnswer() {
     errorMessage.innerHTML = "";
 
     switch (selectMethod.value) {
-      case '1':
+      case "1":
         result = bruteForce(matrix);
         break;
-      case '2':
+      case "2":
         result = nearestNeighbor(matrix);
         break;
-      case '3':
+      case "3":
         result = nearestNeighbor2(matrix);
         break;
     }
 
     drawGraph();
-    resultField.innerHTML = generateAnswer(result)
+    resultField.innerHTML = generateAnswer(result);
   }
 }
 
 //Считывание матрицы
 function readMatrix(r, c) {
-  errorMessage.innerHTML = '';
+  errorMessage.innerHTML = "";
 
   if (!matrix[r]) {
     matrix[r] = new Array();
@@ -305,14 +309,13 @@ function readMatrix(r, c) {
 
   matrixInput[c][r].value = matrixInput[r][c].value;
   matrix[r][c] = matrixInput[r][c].value;
-  matrix[c][r] = matrixInput[c][r].value
+  matrix[c][r] = matrixInput[c][r].value;
 
-  if (matrix[r][c] == '') {
+  if (matrix[r][c] == "") {
     matrixCountElements -= 2;
   } else {
     matrixCountElements += 2;
   }
-
 }
 
 //Создание полей для матрицы
@@ -321,84 +324,83 @@ function createMatrixInputs() {
   matrixCountElements = cityCount;
 
   if (!cityCount) {
-    matrixInputs.innerHTML = '';
+    matrixInputs.innerHTML = "";
   }
 
   if (cityCount < 3) {
-    errorMessage.innerHTML = 'Количество городов не может быть меньше 3';
+    errorMessage.innerHTML = "Количество городов не может быть меньше 3";
   }
 
   if (cityCount > 30) {
-    errorMessage.innerHTML = 'Количество городов не должно превышать 20';
+    errorMessage.innerHTML = "Количество городов не должно превышать 20";
   }
 
   if (cityCount && cityCount >= 3 && cityCount <= 30) {
-    errorMessage.innerHTML = '';
-    matrixInputs.innerHTML = '';
+    errorMessage.innerHTML = "";
+    matrixInputs.innerHTML = "";
 
     for (let i = 0; i < cityCount; i++) {
-      matrixInputsRow[i] = document.createElement('div');
+      matrixInputsRow[i] = document.createElement("div");
       matrixInputs.appendChild(matrixInputsRow[i]);
       matrixInput[i] = new Array(4);
 
       for (let j = 0; j < cityCount; j++) {
-        matrixInput[i][j] = document.createElement('input');
+        matrixInput[i][j] = document.createElement("input");
         matrixInput[i][j].setAttribute("type", "number");
 
         if (i == j) {
           matrixInput[i][j].setAttribute("disabled", "disabled");
-          matrixInput[i][j].classList.add('blue-input');
+          matrixInput[i][j].classList.add("blue-input");
         }
 
-        matrixInput[i][j].addEventListener('input', () => readMatrix(i, j));
+        matrixInput[i][j].addEventListener("input", () => readMatrix(i, j));
         matrixInputsRow[i].appendChild(matrixInput[i][j]);
       }
     }
   }
 }
 
-function cleanForm() {
-}
+function cleanForm() {}
 
 function taskPage() {
-  aboutSection.classList.add('hidden');
-  methodsSection.classList.add('hidden');
-  mainPageMethodsSection.classList.add('hidden');
-  taskSection.classList.remove('hidden');
-  mainPageBtn.classList.remove('red-text');
-  methodsPageBtn.classList.remove('red-text');
-  solvePageBtn.classList.add('red-text');
+  aboutSection.classList.add("hidden");
+  methodsSection.classList.add("hidden");
+  mainPageMethodsSection.classList.add("hidden");
+  taskSection.classList.remove("hidden");
+  mainPageBtn.classList.remove("red-text");
+  methodsPageBtn.classList.remove("red-text");
+  solvePageBtn.classList.add("red-text");
   window.scroll(0, 0);
 }
 
 function methodsPage() {
-  aboutSection.classList.add('hidden');
-  mainPageMethodsSection.classList.add('hidden');
-  methodsSection.classList.remove('hidden');
-  taskSection.classList.add('hidden');
-  mainPageBtn.classList.remove('red-text');
-  methodsPageBtn.classList.add('red-text');
-  solvePageBtn.classList.remove('red-text');
+  aboutSection.classList.add("hidden");
+  mainPageMethodsSection.classList.add("hidden");
+  methodsSection.classList.remove("hidden");
+  taskSection.classList.add("hidden");
+  mainPageBtn.classList.remove("red-text");
+  methodsPageBtn.classList.add("red-text");
+  solvePageBtn.classList.remove("red-text");
   window.scroll(0, 0);
 }
 
-mainPageBtn.addEventListener('click', () => {
+mainPageBtn.addEventListener("click", () => {
   window.location.reload();
 });
 
-solvePageBtn.addEventListener('click', taskPage);
+solvePageBtn.addEventListener("click", taskPage);
 
-methodsPageBtn.addEventListener('click', methodsPage);
+methodsPageBtn.addEventListener("click", methodsPage);
 
-cityCountInput.addEventListener('input', createMatrixInputs);
+cityCountInput.addEventListener("input", createMatrixInputs);
 
-btnAnswer.addEventListener('click', getAnswer);
+btnAnswer.addEventListener("click", getAnswer);
 
-btnClean.addEventListener('click', cleanForm);
+btnClean.addEventListener("click", cleanForm);
 
 useMethodBtns.forEach((item) => {
-  item.addEventListener('click', e => {
+  item.addEventListener("click", (e) => {
     taskPage(e);
     selectMethod.value = e.target.dataset.method;
   });
-}); 
+});
